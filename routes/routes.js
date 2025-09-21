09fea24e0177a19a3058cef5aa2ce092a37b7cda
custapq <teeradon88@gmail.com>
2025-09-21 12:01:50 +0700

    update readme about how to setup and start

--- routes/routes.js
+++ routes/routes.js
@@ -8,30 +8,29 @@ import {addSubjectStructure, getAllSubjectStructure} from "../controllers/subjec
 const router = express.Router();
 
 //api ในการจัดการการลงทะเบียนวิชา
-router.post("/register", registeredSubject);
-router.post("/registerSubject", registerSubject);
-router.put("/updateGrade", changeGrade);
+router.post("/register", registeredSubject); // insert ข้อมูลการลงทะเบียนวิชา
+router.post("/registerSubject", registerSubject); // ลงทะเบียนวิชาของนักเรียน
+router.put("/updateGrade", changeGrade); // update เกรดของนักเรียน
 
 //api ในการจัดการ student
-router.post("/students", createStudentController);
-router.get("/students", getAllStudent);
-router.get("/students/:id", getStudentDetail);
-router.get("/students/admin/:id", getStudentDetailAdmin);
+router.post("/students", createStudentController); // insert ข้อมูลนักเรียน
+router.get("/students", getAllStudent); // ดึงข้อมูลนักเรียนทั้งหมด
+router.get("/students/:id", getStudentDetail); // ดึงข้อมูลนักเรียนโดยใช้ student_id
+router.get("/students/admin/:id", getStudentDetailAdmin); // ดึงข้อมูลนักเรียนโดยใช้ student_id เพื่อแสดงผลใน ADMIN
 
 //api ในการจัดการ subject
-router.post("/subjects", addSubject);
-router.get("/subjects/:student_id", getAllSubject);
-router.get("/subjects/notRegistered/:student_id", getAllNotRegisteredSubject);
-
+router.post("/subjects", addSubject); // insert ข้อมูลรายวิชา
+router.get("/subjects/:student_id", getAllSubject);     // ดึงข้อมูลรายวิชาทั้งหมดของนักเรียนคนนั้นๆ
+router.get("/subjects/notRegistered/:student_id", getAllNotRegisteredSubject); // ดึงข้อมูลรายวิชาที่ยังไม่ลงทะเบียนของนักเรียนคนนั้นๆ
 //api ในการจัดการ user
-router.post("/users/register", registerUser);
-router.post("/users/login/admin", loginUserAdmin);
-router.post("/users/login/student", loginUserStudent);
-router.get("/users", getAllUers);
+router.post("/users/register", registerUser); // insert ข้อมูล user ใหม่
+router.post("/users/login/admin", loginUserAdmin); // login user admin
+router.post("/users/login/student", loginUserStudent); // login user student
+router.get("/users", getAllUers); // ดึงข้อมูล user ทั้งหมด
 
 //api ในการจัดการ subject structure
-router.post("/subjectStructures", addSubjectStructure);
-router.get("/subjectStructures", getAllSubjectStructure);
+router.post("/subjectStructures", addSubjectStructure); // insert ข้อมูลโครงสร้างรายวิชา
+router.get("/subjectStructures", getAllSubjectStructure); // ดึงข้อมูลโครงสร้างรายวิชาทั้งหมด
 
 //view rendering
 router.get("/", (req, res) => {
